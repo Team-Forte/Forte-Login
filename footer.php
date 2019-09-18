e48f834c1cb418153e7725a061c5a3b8fbcc654c
Victor Ezeh <ezehgvictor@gmail.com>
2019-09-19 00:20:55 +0100

    successfully converted html files to php files

--- footer.php
+++ footer.php
@@ -1,1 +1,11 @@
-footer.php
+<footer>
+	<p>
+		<a target="_blank" href="#">Forte Tech Institute</a>
+	
+	</p>
+</footer>
+
+<script src="https://kit.fontawesome.com/b94aae637f.js"></script>
+<script type="text/javascript" src="assets/js/cindy.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,53 @@
-auth.php
+
+<?php include('header.php'); ?>
+
+<body>
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+
+		<form action="#">
+			<h1>Create Account</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your email for registration</span>
+			<input type="text" placeholder="Name" />
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<button>Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign in to Forte</h1>
+			<div class="social-container">
+				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your account</span>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<div class="boxed"><a href="#">Forgot your password?</a></div>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h2>Welcome Back!</h2>
+				<p>To keep connected with us please<br>login with your personal info</p>
+				<button class="ghost" id="signIn">Sign In</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h2>Hello, Friend!</h2>
+				<p>Enter your personal details<br>and start journey with us</p>
+				<button class="ghost" id="signUp">Sign Up</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Note: Closing body tag is including in the footer.html file -->
+<?php include('footer.php'); ?>
