edad94d436c81fb9b3bea5974f7da0dc00c91f2a
ChiUba-Eze <50292866+ChiUba-Eze@users.noreply.github.com>
2019-09-19 21:32:25 +0100

    phpscripts

--- index.php
+++ index.php
@@ -1,11 +1,27 @@
-<?php include('db_con.php'); ?>
-<?php include('header.php'); ?>
+<?php
 
-<body>
-<div class="container" id="container">
+require_once "script/functions.php";
+require_once "script/register.php";
+require_once "script/login.php";
+?>
+
+
+<!DOCTYPE html>
+  <html style="width: 100%; height: 100%;" lang="en">
+    <head>
+          <?php echo head('Forte Tech');     ?>     
+    </head>
+    
+    <body>
+
+
+ <div class="container" id="container">
 	<div class="form-container sign-up-container">
+	  
+	  
+	 
 
-		<form id="register" method="POST">
+		<form id="userInfo" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
 			<h1>Create Account</h1>
 			<div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
@@ -13,14 +29,30 @@
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div>
 			<span>or use your email for registration</span>
-			<input type="text" name="name" placeholder="Name" required />
-			<input type="email" name="email" placeholder="Email" required />
-			<input type="password" name="password" placeholder="Password" required />
-			<button>Sign Up</button>
+                        
+                        <?php  echo $suss; ?>
+                        <span id="Error1" style="font-size: 12px;color:red;margin-left: 20px;"><?php echo $errorname; ?></span><br/>
+			<input type="text" name="name" id="name" value="<?php echo $userName; ?>" placeholder="Name" />
+                        
+ 
+                        
+                        <span id="Error2" style="font-size: 12px;color:red;margin-left: 20px;"><?php echo $erroremail; ?></span><br/>
+			<input type="email" name="email"  value="<?php echo $userEmail; ?>" id="email" placeholder="Email" />
+                        
+                        
+                        
+                        <span id="Error3" style="font-size: 12px;color:red;margin-left: 20px;"><?php echo $errorpassword; ?></span><br/>
+			<input type="password" name="password" value="<?php echo $userPassword; ?>" id="password" placeholder="Password" />
+			
+                        
+                        
+                        <button id= "signup" type="submit" name="register">Sign Up</button>
 		</form>
 	</div>
-	<div class="form-container sign-in-container">
-		<form id="login" method="POST">
+        
+        
+        <div class="form-container sign-in-container">
+		<form id="userInfo2" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
 			<h1>Sign in to Forte</h1>
 			<div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
@@ -28,27 +60,53 @@
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div>
 			<span>or use your account</span>
-			<input type="email" name="email" placeholder="Email" required/>
-			<input type="password" name="password" placeholder="Password" required />
+                        
+                        <span id="Error2" style="font-size: 12px;color:red;margin-left: 20px;"><?php echo $errorlogin; ?> </span><br/>
+			<input type="email" name="email" id="email" placeholder="Email" />
+			
+                        <span id="Error3" style="font-size: 12px;color:red;margin-left: 20px;"></span><br/>
+                        <input type="password" name="password" id="password" placeholder="Password" />
+                        
 			<div class="boxed"><a href="#">Forgot your password?</a></div>
-			<button>Sign In</button>
+                        
+			<button class= "signin" type="submit" name="LOGIN" >Sign In</button>
 		</form>
 	</div>
+        
+        
+	
 	<div class="overlay-container">
 		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h2>Welcome Back!</h2>
-				<p>To keep connected with us please<br>login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
+                    <div class="overlay-panel overlay-right">
 				<h2>Hello, Friend!</h2>
 				<p>Enter your personal details<br>and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+				<button  class="ghost" id="signUp">Sign Up</button>
 			</div>
+                    <div class="overlay-panel overlay-left">
+				<h2>Welcome Back!</h2>
+				<p>To keep connected with us please<br>login with your personal info</p>
+				<button  class="ghost" id="signIn">Sign In</button>
+				
+			</div>
+			
+			
 		</div>
 	</div>
 </div>
 
-<!-- Note: Closing body tag is including in the footer.html file -->
-<?php include('footer.php'); ?>
+
+<footer>
+	<p>
+		<a target="_blank" href="#">Forte Tech Institute</a>
+	
+	</p>
+</footer>
+
+<script src="https://kit.fontawesome.com/b94aae637f.js"></script>
+<script type="text/javascript" src="script/cindy.js"></script>
+
+
+</body>
+</html>
+
+
--- register.php
+++ register.php
@@ -1,24 +1,78 @@
 <?php
-		include('db_con.php');
-		
-		$name =mysqli_real_escape_string($conn, $_POST['name']);
-		$email = mysqli_real_escape_string($conn, $_POST['email']);
+require_once "script/functions.php";
 
-		$password1 = mysqli_real_escape_string($conn,$_POST['password']);
+$errorname=''; $erroremail=''; $errorpassword='';
+$userName=''; $userEmail=''; $userPassword=''; $suss='';
 
-		// first check the database to make sure 
-  // a user does not already exist with the same email
-  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
-  $result = mysqli_query($conn, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-  
-  if ($user) { // if user exists
-    	echo "false";
-    }else{
-    $password = md5($password1);
-	$query = "INSERT INTO users (name, email, password)VALUES ('$name', '$email', '$password')";
-		 mysqli_query($conn,$query)or die(mysqli_error($conn));
-		echo "success";
+if(isset($_POST['register'])){
+    
+    // declare form varible
+$userName = trim($_POST['name']);
+$userEmail = trim($_POST['email']);
+$userPassword = trim($_POST['password']); 
 
+if(strlen($userName) <=0){  
+    $errorname = 'Input a name';
+}elseif(!is_string($userName)) {
+    $errorname = 'Only letters is required';
 }
-		?>
+
+if(!filter_var($userEmail, FILTER_VALIDATE_EMAIL)){  
+    $erroremail = 'Input a valid email';
+}
+
+if(strlen($userPassword) <=6){  
+    $errorpassword = 'Password must be at least 6 charaters';
+}
+
+
+
+//check if credentials is clean
+if( !(strlen($errorname) >4) && !(strlen($erroremail) >4) && !(strlen($errorpassword) >4) ){
+   
+  $conn=connecTDatabase();          //connect to database
+  
+  
+$st = $conn->prepare("SELECT email FROM users
+	WHERE email = ? "); //query database
+	$st->execute(array($userEmail));
+$emailcount=$st->rowCount();
+$conn=null; 
+if($emailcount <= 0){
+  $conn=connecTDatabase();          //connect to database  
+    try{
+    $st = $conn->prepare("INSERT INTO users(name,email,password)
+      VALUES(:userName,:userEmail,:userPassword)");
+    
+      $st->bindParam(':userName', $userName);
+      $st->bindParam(':userEmail', $userEmail);
+      $st->bindParam(':userPassword', $userPassword);
+      
+      $st->execute();
+$conn=null; 
+      } catch (PDOException $e) {
+           print "Database distress. Retry!: " . $e->getMessage() . "<br/>";
+           die();
+        } 
+$userName=''; $userEmail=''; $userPassword=''; $suss=''; 
+}else{
+    $errorname= 'Email Exists';
+}
+
+   
+}
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+?>
